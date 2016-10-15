4b7e66b2711378e8dc9065be74d770b920664417
Nils Wagenaar <n.w.wagenaar@students.uu.nl>
2016-10-15 14:51:26 +1100

    addition lat lon subset

--- PCRGLOB-WB.py
+++ PCRGLOB-WB.py
@@ -1,13 +1,22 @@
-import numpy
+import numpy as np
 import netCDF4
 import matplotlib.pyplot
 
 GlobRunoff=netCDF4.Dataset('PCRGLOB-WB1979-2012Runoff.nc', 'r')
 #reads all PCRGLOB-WB data
 print (GlobRunoff)
-VariableRunoff=GlobRunoff.variables['Runoff']
-TestRunoff=VariableRunoff[100, 100]
-print (TestRunoff)
-image=matplotlib.pyplot.imshow(TestRunoff)
+latbounds=[ 40 , 43 ]
+lonbounds=[ -96 , -89 ]
+lats=GlobRunoff.variables['lat'][:]
+lons=GlobRunoff.variables['lon'][:]
+#latitude and longitude closest to bounds is found by
+latli=np.argmin( np.abs( lats - latbounds[0]))
+latui=np.argmin( np.abs( lats - latbounds[1]))
+lonli=np.argmin( np.abs( lons - lonbounds[0]))
+lonui=np.argmin( np.abs( lons - lonbounds[1]))
+#subset
+VariableRunoffsubset=GlobRunoff.variables['Runoff'][ 1 , latli:latui , lonli:lonui ]
+print (VariableRunoffsubset)
+image=matplotlib.pyplot.imshow(VariableRunoffsubset)
 matplotlib.pyplot.show()
 
